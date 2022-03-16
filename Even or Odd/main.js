f843308a6e2098d86e4c638d670da9d0bda1beb4
GregTorrillo <99836116+GregTorrillo@users.noreply.github.com>
2022-03-16 15:06:24 -0400

    Create main.js

---
+++ Even or Odd/main.js
@@ -0,0 +1,13 @@
+// Create a function that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers.
+
+function evenOfOdd(number) {
+  if (number % 2 == 0) {
+    return "Even";
+  } else {
+    return "Odd";
+  }
+}
+
+function evenOrOdd(number) {
+  return number % 2==0 ? 'Even' : 'Odd'
+}
