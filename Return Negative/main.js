10695df980e58ea4615a0eb19f6839872098107d
GregTorrillo <99836116+GregTorrillo@users.noreply.github.com>
2022-03-18 15:29:04 -0400

    Function return negative number

--- Return Negative/main.js
+++ Return Negative/main.js
@@ -1,1 +1,10 @@
+// Given a number and have to make it negative. But maybe the number is already negative?
 
+function makeNegative(num) {
+  if ( num > 0 ) {
+        return -num;
+    }
+    else {
+        return num;
+    }
+}
