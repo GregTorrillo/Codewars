b6d4d68b10c354c6160d8a9ba8c3d864ee505e4e
GregTorrillo <99836116+GregTorrillo@users.noreply.github.com>
2022-03-18 16:04:45 -0400

    Reverse string

--- Reversed Strings/main.js
+++ Reversed Strings/main.js
@@ -1,1 +1,12 @@
+// Complete the solution so that it reverses the string passed into it.
 
+function solution(str){
+  //convert the string to an array
+       let array = str.split("");
+
+    //Use the reverse method
+    array.reverse()
+
+    //Convert it back to a string and return
+    return array.join("")
+}
