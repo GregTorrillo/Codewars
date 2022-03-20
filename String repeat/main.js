204ad4c1bb78e24e4c06fdec924093aea4b4fde1
GregTorrillo <99836116+GregTorrillo@users.noreply.github.com>
2022-03-19 20:49:29 -0400

    Repeat()method

--- String repeat/main.js
+++ String repeat/main.js
@@ -1,1 +1,6 @@
+//Write a function called repeatStr which repeats the given string string exactly n times.
 
+
+const repeatStr = (n, s) => s.repeat(n)           /*built in method, repeat()method constructs and returns a new string
+                                                  which contains the specified number of copies of the string on which 
+                                                  it was called, concatenated together.*/
