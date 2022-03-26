756aaf63feffb4de8bbd62a0046b6fd492d86af9
GregTorrillo <99836116+GregTorrillo@users.noreply.github.com>
2022-03-26 16:46:19 -0400

    for loop, forEach, filter, reduce

--- Counting sheep/main.js
+++ Counting sheep/main.js
@@ -1,7 +1,7 @@
 
 //Consider an array/list of sheep where some sheep may be missing from their place. We need a function that counts the number of sheep present in the array (true means present).
 
-//loop
+//for loop
 function countSheeps(arrayOfSheep) {
    let counter = 0
     for(let i = 0; i < arrayOfSheep.length; i++) {
