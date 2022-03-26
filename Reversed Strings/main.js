edea48745cadc4532f2d2f6f6c4b3db9d2fbf9d7
GregTorrillo <99836116+GregTorrillo@users.noreply.github.com>
2022-03-26 18:43:22 -0400

    str.split(''), array.reverse(), array.join('')

--- Reversed Strings/main.js
+++ Reversed Strings/main.js
@@ -4,9 +4,9 @@ function solution(str){
   //convert the string to an array
        let array = str.split("");
 
-    //Use the reverse method
+    //use the reverse method
     array.reverse()
 
-    //Convert it back to a string and return
+    //convert it back to a string and return
     return array.join("")
 }
