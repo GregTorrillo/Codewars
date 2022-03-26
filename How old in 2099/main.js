0bd291e13706cd5e80bef2101437cc0523eb0476
GregTorrillo <99836116+GregTorrillo@users.noreply.github.com>
2022-03-26 18:23:40 -0400

    year-birthYear conditional

--- How old in 2099/main.js
+++ How old in 2099/main.js
@@ -12,11 +12,11 @@ function  calculateAge(birthYear,year) {
    age = year-birthYear;
    if(age === 1)
      return "You are 1 year old.";
-   else if(age===-1)
+   else if(age === -1)
      return "You will be born in 1 year."
-   else if(age>0)
+   else if(age > 0)
      return "You are " + age + " years old.";
-   else if(age<0) {
+   else if(age < 0) {
      age = birthYear-year;
      return  "You will be born in " + age + " years."
    }
