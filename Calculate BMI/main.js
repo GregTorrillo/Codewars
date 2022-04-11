d290034661a4e8a7751b6bd9da63042911c4d71f
GregTorrillo <99836116+GregTorrillo@users.noreply.github.com>
2022-04-10 22:34:25 -0400

    Update main.js

--- Calculate BMI/main.js
+++ Calculate BMI/main.js
@@ -1,1 +1,28 @@
+//CalculateBMI
+/*  Write function bmi that calculates body mass index (bmi = weight / height2).
 
+if bmi <= 18.5 return "Underweight"
+
+if bmi <= 25.0 return "Normal"
+
+if bmi <= 30.0 return "Overweight"
+
+if bmi > 30 return "Obese"
+*/
+
+function bmi(weight, height) {
+  if(weight/height **2 <= 18.5){
+    return 'Underweight'
+  }if(weight/height **2 <= 25.0){
+    return 'Normal'
+  }if(weight/height **2 <= 30.0){
+    return 'Overweight'
+}if(weight/height **2 > 30.0){
+    return 'Obese'
+  }
+}
+
+function bmi(weight, height) {
+  let b = weight / height**2;
+  return b <= 18.5 ? 'Underweight' : b <= 25.0 ? 'Normal' : b <= 30.0 ? 'Overweight' : 'Obese';
+}
