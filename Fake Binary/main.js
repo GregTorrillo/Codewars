05706415e0939ce82e3e945dc256d31e64aa90d4
GregTorrillo <99836116+GregTorrillo@users.noreply.github.com>
2022-04-12 22:33:07 -0400

    Update main.js

--- Fake Binary/main.js
+++ Fake Binary/main.js
@@ -1,1 +1,21 @@
+//Fake Binary
+//Given a string of digits, you should replace any digit below 5 with '0' and any digit 5 and above with '1'. Return the resulting string.
+
+function fakeBin(str){
+  let newStr = "";
+  for(let i=0;i<str.length;i++){
+    if(Number(str[i])>=5){
+      newStr += "1"
+    }
+    else{
+      newStr += "0";
+    }
+  }
+  return newStr;
+}
+
+
+function fakeBin(x){
+  return x.replace(/[1234]/g, '0').replace(/[56789]/g, '1')
+}
 
