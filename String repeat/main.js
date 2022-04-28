b1d84b4f0c6f1bbb16f4e0b16989b9548c99ec58
GregTorrillo <99836116+GregTorrillo@users.noreply.github.com>
2022-04-28 18:42:59 -0400

    Delete String repeat directory

--- String repeat/main.js
+++
@@ -1,7 +0,0 @@
-//Write a function called repeatStr which repeats the given string exactly n times.
-
-function repeatStr (n, s) {
-  return s.repeat(n);
-}                                                /*built in method, repeat()method constructs and returns a new string
-                                                  which contains the specified number of copies of the string on which 
-                                                  it was called, concatenated together.*/
