815ac56c2c829a5ce4e04ee93e1f454e9fa4e208
GregTorrillo <99836116+GregTorrillo@users.noreply.github.com>
2022-04-27 21:48:45 -0400

    Create Reversed Strings.js

---
+++ 8kyu/Reversed Strings.js
@@ -0,0 +1,12 @@
+// Complete the solution so that it reverses the string passed into it.
+
+function solution(str){
+  //convert the string to an array
+       let array = str.split("");
+
+    //use the reverse method
+    array.reverse()
+
+    //convert it back to a string and return
+    return array.join("")
+}
