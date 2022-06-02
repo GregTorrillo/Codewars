df217d68b1374992dac6311c1b345c6abbd6ba1a
GregTorrillo <99836116+GregTorrillo@users.noreply.github.com>
2022-06-01 21:32:27 -0400

    Create Sum Mixed Array.js

---
+++ 8kyu/Sum Mixed Array.js
@@ -0,0 +1,18 @@
+/*
+Given an array of integers as strings and numbers, return the sum of the array values as if all were numbers.
+
+Return your answer as a number.
+
+FUNDAMENTALSSTRINGSARRAYS
+*/
+
+function sumMix(x){
+  let sum = 0;
+  for (i = 0; i<x.length; i++) {
+    sum += Number(x[i]);
+  }
+  return sum;
+}
+
+//reduce method
+sumMix = x => x.reduce((prev,curr) => +curr + prev, 0)
