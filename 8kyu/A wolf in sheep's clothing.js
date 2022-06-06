38f023c0d08de46bee8f78828ea7c4a3ce141e2b
GregTorrillo <99836116+GregTorrillo@users.noreply.github.com>
2022-06-06 18:23:24 -0400

    Update A wolf in sheep's clothing.js

--- 8kyu/A wolf in sheep's clothing.js
+++ 8kyu/A wolf in sheep's clothing.js
@@ -14,8 +14,7 @@ Output: "Oi! Sheep number 1! You are about to be eaten by a wolf!"
 
 Input: ["sheep", "sheep", "wolf"]
 Output: "Pls go away and stop eating my sheep"
-
-FUNDAMENTALSARRAYSLOOPSCONTROL FLOWBASIC LANGUAGE FEATURES*/
+*/
 
 function warnTheSheep(queue) {
   let sheepPos = queue.length - queue.indexOf('wolf') -1
