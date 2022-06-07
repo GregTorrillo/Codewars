a7ffd2977c6cb12afa784ca670fe11e2e57fa806
GregTorrillo <99836116+GregTorrillo@users.noreply.github.com>
2022-06-07 17:35:50 -0400

    Update Playing Banjo.js

--- 8kyu/Playing Banjo.js
+++ 8kyu/Playing Banjo.js
@@ -4,14 +4,14 @@ The function takes a name as its only argument, and returns one of the following
 
 function areYouPlayingBanjo(name) {
  if (name[0] === 'R' || name[0] === 'r') {
-    return `${name} plays banjo`;
+    return `${name} plays banjo`
   } else {
-    return `${name} does not play banjo`;
+    return `${name} does not play banjo`
   }
-  return name;
+  return name
 }
 
 
 function areYouPlayingBanjo(name) {
-  return name + (name[0].toLowerCase() == 'r' ? ' plays' : ' does not play') + " banjo";
+  return name + (name[0].toLowerCase() == 'r' ? ' plays' : ' does not play') + " banjo"
 }
