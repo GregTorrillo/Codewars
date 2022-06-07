da2642aa2c4c4b4a335f2d62f791d6853d428e7e
GregTorrillo <99836116+GregTorrillo@users.noreply.github.com>
2022-06-07 17:32:34 -0400

    Update You only need one - Beginner.js

--- 8kyu/You only need one - Beginner.js
+++ 8kyu/You only need one - Beginner.js
@@ -4,8 +4,6 @@ You will be given an array a and a value x. All you need to do is check whether 
 Array can contain numbers or strings. X can be either.
 
 Return true if the array contains the value, false if not.
-
-FUNDAMENTALSBOOLEANSSTRINGSNUMBERSARRAYS
 */
 
 
