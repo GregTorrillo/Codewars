1caf1c6ae23b3cfaa8a7492088ade93c67b58245
GregTorrillo <99836116+GregTorrillo@users.noreply.github.com>
2022-06-09 17:56:26 -0400

    Update Basic Mathematical Operations.js

--- 8kyu/Basic Mathematical Operations.js
+++ 8kyu/Basic Mathematical Operations.js
@@ -1,6 +1,8 @@
-/*our task is to create a function that does four basic mathematical operations.
+/*
+Our task is to create a function that does four basic mathematical operations.
 The function should take three arguments - operation(string/char), value1(number), value2(number).
-The function should return result of numbers after applying the chosen operation.*/
+The function should return result of numbers after applying the chosen operation.
+*/
 
 function basicOp(operation, value1, value2){
   if( operation == '+') {
