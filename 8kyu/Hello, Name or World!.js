6cd7a26a0149f092cece4bd8978154607315a31f
GregTorrillo <99836116+GregTorrillo@users.noreply.github.com>
2022-06-12 16:59:18 -0400

    Update Hello, Name or World!.js

--- 8kyu/Hello, Name or World!.js
+++ 8kyu/Hello, Name or World!.js
@@ -1,4 +1,5 @@
-/*Define a method hello that returns "Hello, Name!" to a given name, or says Hello, World! if name is not given (or passed as an empty String).
+/*
+Define a method hello that returns "Hello, Name!" to a given name, or says Hello, World! if name is not given (or passed as an empty String).
 
 Assuming that name is a String and it checks for user typos to return a name with a first capital letter (Xxxx).
 
@@ -8,7 +9,7 @@ Examples:
 * With `name` = "aliCE" => return "Hello, Alice!"
 * With `name` not given 
   or `name` = ""        => return "Hello, World!"
-FUNDAMENTALS*/
+*/
 
 function hello(name) {
   if (name == null || name == ''){
