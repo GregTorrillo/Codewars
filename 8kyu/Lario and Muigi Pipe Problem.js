6e64504a76ca2094bb5be1e2bc17b71aa7588abb
GregTorrillo <99836116+GregTorrillo@users.noreply.github.com>
2022-06-12 16:47:21 -0400

    Update Lario and Muigi Pipe Problem.js

--- 8kyu/Lario and Muigi Pipe Problem.js
+++ 8kyu/Lario and Muigi Pipe Problem.js
@@ -1,5 +1,4 @@
 /*
-ssue
 Looks like some hoodlum plumber and his brother has been running around and damaging your stages again.
 
 The pipes connecting your level's stages together need to be fixed before you receive any more complaints.
@@ -11,8 +10,6 @@ Given the a list of numbers, return a fixed list so that the values increment by
 
 Example
 Input: 1,3,5,6,7,8 Output: 1,2,3,4,5,6,7,8
-
-FUNDAMENTALSLOOPSCONTROL FLOWBASIC LANGUAGE FEATURESLISTSDATA STRUCTURESOBJECTS
 */
 
 function pipeFix(numbers){
