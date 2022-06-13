dd3c7871af78b8692da5f14c3a0bf5d8c8d5f274
GregTorrillo <99836116+GregTorrillo@users.noreply.github.com>
2022-06-13 14:30:13 -0400

    Update Calculate BMI.js

--- 8kyu/Calculate BMI.js
+++ 8kyu/Calculate BMI.js
@@ -1,5 +1,5 @@
-//CalculateBMI
-/*  Write function bmi that calculates body mass index (bmi = weight / height2).
+/*  
+Write function bmi that calculates body mass index (bmi = weight / height2).
 
 if bmi <= 18.5 return "Underweight"
 
