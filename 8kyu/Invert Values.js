f67356b4c6a39d12102e33b4c9afae9463ea2bde
GregTorrillo <99836116+GregTorrillo@users.noreply.github.com>
2022-06-13 15:48:26 -0400

    Update Invert Values.js

--- 8kyu/Invert Values.js
+++ 8kyu/Invert Values.js
@@ -1,15 +1,18 @@
-//Invert values
-//Given a set of numbers, return the additive inverse of each. Each positive becomes negatives, and the negatives become positives.
+/*
+Given a set of numbers, return the additive inverse of each. Each positive becomes negatives, and the negatives become positives.
 
+invert([1,2,3,4,5]) == [-1,-2,-3,-4,-5]
+invert([1,-2,3,-4,5]) == [-1,2,-3,4,-5]
+invert([]) == []
 
- /* 
-step one: go through each number in the array using the map method
-step two: multiply each number by -1
-step three: return new array
+You can assume that all values are integers. Do not mutate the input array/list.
 */
 
 function invert(array) {
-   return array.map(num =>   {
+//step one: go through each number in the array using the map method
+   return array.map(num =>   { 
+    //step two: multiply each number by -1
+    //step three: return new array
      return num * -1
    })
 }
