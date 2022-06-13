d0e339b039bce0d459f87e7d9c2a21f8785393c1
GregTorrillo <99836116+GregTorrillo@users.noreply.github.com>
2022-06-13 15:29:44 -0400

    Update Calculate Average.js

--- 8kyu/Calculate Average.js
+++ 8kyu/Calculate Average.js
@@ -1,5 +1,5 @@
-//Calculate average
 //Write a function which calculates the average of the numbers in a given list.
+
 function find_average(array) {
   let sum = 0;
   for (let i = 0; i < array.length; i++){
