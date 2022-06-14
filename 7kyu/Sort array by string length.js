2715bd649e4d16491bcaa6cca5abe0a8f3f2ed19
GregTorrillo <99836116+GregTorrillo@users.noreply.github.com>
2022-06-14 15:36:14 -0400

    Update Sort array by string length.js

--- 7kyu/Sort array by string length.js
+++ 7kyu/Sort array by string length.js
@@ -1,4 +1,5 @@
-/*Write a function that takes an array of strings as an argument and returns a sorted array containing the same strings, ordered from shortest to longest.
+/*
+Write a function that takes an array of strings as an argument and returns a sorted array containing the same strings, ordered from shortest to longest.
 
 For example, if this array were passed as an argument:
 
@@ -9,8 +10,7 @@ Your function would return the following array:
 ["Eyes", "Glasses", "Monocles", "Telescopes"]
 
 All of the strings in the array passed to your function will be different lengths, so you will not have to decide how to order multiple strings of the same length.
-
-FUNDAMENTALSSORTINGALGORITHMSARRAYS*/
+*/
 
 function sortByLength (array) {
     return array.sort((a,b) => a.length - b.length)
