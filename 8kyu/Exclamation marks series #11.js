3032abd07caaac6a9b1ba0c8d05f46284d071d92
GregTorrillo <99836116+GregTorrillo@users.noreply.github.com>
2022-06-14 15:17:58 -0400

    Update Exclamation marks series #11.js

--- 8kyu/Exclamation marks series #11.js
+++ 8kyu/Exclamation marks series #11.js
@@ -1,11 +1,12 @@
-/*Replace all vowel to exclamation mark in the sentence. aeiouAEIOU is vowel.
+/*
+Replace all vowel to exclamation mark in the sentence. aeiouAEIOU is vowel.
 
 Examples
 replace("Hi!") === "H!!"
 replace("!Hi! Hi!") === "!H!! H!!"
 replace("aeiou") === "!!!!!"
 replace("ABCDE") === "!BCD!"
-FUNDAMENTALS*/
+*/
 
 function replace(s){
   let vowels = ['a','e','i','o','u','A','E','I','O','U']
