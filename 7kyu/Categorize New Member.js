809e5283efc2885aaa6b5ebadbf5b3a40eb091a7
GregTorrillo <99836116+GregTorrillo@users.noreply.github.com>
2022-07-10 23:34:48 -0400

    Update Categorize New Member.js

--- 7kyu/Categorize New Member.js
+++ 7kyu/Categorize New Member.js
@@ -9,7 +9,7 @@ Input will consist of a list of pairs. Each pair contains information for a sing
 Output
 Output will consist of a list of string values (in Haskell and C: Open or Senior) stating whether the respective member is to be placed in the senior or open category.
 
-Example
+Ex:
 input =  [[18, 20], [45, 2], [61, 12], [37, 6], [21, 21], [78, 9]]
 output = ["Open", "Open", "Senior", "Open", "Open", "Senior"]
 */
