26a63e515ba6ab1c0b888476d9c7f5d257d8cf58
GregTorrillo <99836116+GregTorrillo@users.noreply.github.com>
2022-07-21 18:42:09 -0400

    Update What is between?.js

--- 8kyu/What is between?.js
+++ 8kyu/What is between?.js
@@ -1,6 +1,6 @@
 /*Complete the function that takes two integers (a, b, where a < b) and return an array of all integers between the input parameters, including them.
 
-For example:
+Example:
 
 a = 1
 b = 4
