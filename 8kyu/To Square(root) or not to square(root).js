543c9c6c03a3c19b5ec24edd9c55a44361cb45cc
GregTorrillo <99836116+GregTorrillo@users.noreply.github.com>
2022-07-29 18:45:22 -0400

    Update To Square(root) or not to square(root).js

--- 8kyu/To Square(root) or not to square(root).js
+++ 8kyu/To Square(root) or not to square(root).js
@@ -7,7 +7,7 @@ If the number has an integer square root, take this, otherwise square the number
 
 Example
 [4,3,9,7,2,1] -> [2,9,3,49,4,1]
-Notes
+
 The input array will always contain only positive numbers, and will never be empty or null.
 */
 
