06a87d3e6368a8593b61e2d2623288d6e480df1f
GregTorrillo <99836116+GregTorrillo@users.noreply.github.com>
2022-08-14 20:18:51 -0400

    Update Vowel remover.js

--- 8kyu/Vowel remover.js
+++ 8kyu/Vowel remover.js
@@ -1,7 +1,7 @@
 /*
 Create a function called shortcut to remove the lowercase vowels (a, e, i, o, u ) in a given string.
 
-Examples:
+Example:
 "hello"     -->  "hll"
 "codewars"  -->  "cdwrs"
 "goodbye"   -->  "gdby"
