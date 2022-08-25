8e512f55fd404b3dc976b35690a14751b693619d
GregTorrillo <99836116+GregTorrillo@users.noreply.github.com>
2022-08-24 21:05:54 -0400

    Update Convert String to Number.js

--- 8kyu/Convert String to Number.js
+++ 8kyu/Convert String to Number.js
@@ -1,5 +1,7 @@
-/*We need a function that can transform a string into a number. What ways of achieving this do you know?
-Note: Don't worry, all inputs will be strings, and every string is a perfectly valid representation of an integral number.*/
+/*
+We need a function that can transform a string into a number. What ways of achieving this do you know?
+Note: Don't worry, all inputs will be strings, and every string is a perfectly valid representation of an integral number.
+*/
 
 let stringToNumber = function(str){
   
