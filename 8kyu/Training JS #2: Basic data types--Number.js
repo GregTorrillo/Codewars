a0666ca3013ebc201cea7959615462a66c7966a3
GregTorrillo <99836116+GregTorrillo@users.noreply.github.com>
2022-08-27 22:32:13 -0400

    Update Training JS #2: Basic data types--Number.js

--- 8kyu/Training JS #2: Basic data types--Number.js
+++ 8kyu/Training JS #2: Basic data types--Number.js
@@ -3,7 +3,7 @@ In javascript, Number is one of basic data types. It can be positive: 1,2,3, neg
 
 Numbers can use operators such as + - * / %
 
-Task
+Task:
 I've written five function equal1,equal2,equal3,equal4,equal5, defines six global variables v1 v2 v3 v4 v5 v6, every function has two local variables a,b, please set the appropriate value for the two variables(select from v1--v6), making these function return value equal to 100. the function equal1 is completed, please refer to this example to complete the following functions.
 
 When you have finished the work, click "Run Tests" to see if your code is working properly.
