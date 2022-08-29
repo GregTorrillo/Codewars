7818222dbf5372bb436da093e7b3a6bbac779f23
GregTorrillo <99836116+GregTorrillo@users.noreply.github.com>
2022-08-28 21:59:15 -0400

    Update Lario and Muigi Pipe Problem.js

--- 8kyu/Lario and Muigi Pipe Problem.js
+++ 8kyu/Lario and Muigi Pipe Problem.js
@@ -8,7 +8,7 @@ Pipes list is correct when each pipe after the first index is greater (+1) than 
 Task
 Given the a list of numbers, return a fixed list so that the values increment by 1 for each index from the minimum value up to the maximum value (both included).
 
-Example
+Ex:
 Input: 1,3,5,6,7,8 Output: 1,2,3,4,5,6,7,8
 */
 
