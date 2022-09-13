887404ee88a6ffadebac00b65f7007882cf61e2d
GregTorrillo <99836116+GregTorrillo@users.noreply.github.com>
2022-09-12 22:44:03 -0400

    Update Count of positives - sum of negatives.js

--- 8kyu/Count of positives - sum of negatives.js
+++ 8kyu/Count of positives - sum of negatives.js
@@ -1,9 +1,10 @@
-//Count of positives / sum of negatives
-/*Given an array of integers.
+/*
+Given an array of integers.
 
 Return an array, where the first element is the count of positives numbers and the second element is sum of negative numbers. 0 is neither positive nor negative.
 
-If the input is an empty array or is null, return an empty array.*/
+If the input is an empty array or is null, return an empty array.
+*/
 
 function countPositivesSumNegatives(input) {
     if (input === null || input.length < 1) {
