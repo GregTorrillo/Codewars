23868c1b8e5ba7c8e29878114d5ea52a9207f880
GregTorrillo <99836116+GregTorrillo@users.noreply.github.com>
2022-10-06 21:40:56 -0400

    Update 5 without numbers !!.js

--- 8kyu/5 without numbers !!.js
+++ 8kyu/5 without numbers !!.js
@@ -1,6 +1,6 @@
 /*
 Write a function that always returns 5
-Just bear in mind that you can't use any of the following characters:   0123456789*+-/
+Just bear in mind that you can't use any of the following characters   0123456789*+-/
 */
 
 function unusualFive() {
