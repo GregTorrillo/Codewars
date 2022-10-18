a8db716f1e5bac8e9d5a99e811449a368b7e0c3d
GregTorrillo <99836116+GregTorrillo@users.noreply.github.com>
2022-10-18 19:42:34 -0400

    Update Do I get a bonus?.js

--- 8kyu/Do I get a bonus?.js
+++ 8kyu/Do I get a bonus?.js
@@ -3,7 +3,7 @@ It's bonus time in the big city! The fatcats are rubbing their paws in anticipat
 
 Build a function that takes in two arguments (salary, bonus). Salary will be an integer, and bonus a boolean.
 
-If bonus is true, the salary should be multiplied by 10. If bonus is false, the fatcat did not make enough money and must receive only his stated salary.
+If bonus is true, the salary should be multiplied by 10. If bonus is false, the fatcat did not make enough money and must receive only his stated salary
 
 Return the total figure the individual will receive as a string prefixed with "£" (= "\u00A3", JS, Go, Java and Julia), "$" (C#, C++, Ruby, Clojure, Elixir, PHP, Python, Haskell and Lua) or "¥" (Rust).
 */
