e6b31a80428de9250438dd4f1204aea9f4204b7e
GregTorrillo <99836116+GregTorrillo@users.noreply.github.com>
2022-11-18 18:40:33 -0500

    Update Equal sides of array.js

--- 6kyu/Equal sides of array.js
+++ 6kyu/Equal sides of array.js
@@ -3,7 +3,7 @@ You are going to be given an array of integers. Your job is to take that array a
 
 For example:
 
-Let's say you are given the array {1,2,3,4,3,2,1}
+Let's say you are given the array {1,2,3,4,3,2,1}:
 Your function will return the index 3, because at the 3rd position of the array, the sum of left side of the index ({1,2,3}) and the sum of the right side of the index ({3,2,1}) both equal 6.
 
 Let's look at another one
