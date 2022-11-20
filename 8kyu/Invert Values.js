d02ca7b8bc5521bbab5ed7d6e379313d418f32f6
GregTorrillo <99836116+GregTorrillo@users.noreply.github.com>
2022-11-19 20:31:55 -0500

    Update Invert Values.js

--- 8kyu/Invert Values.js
+++ 8kyu/Invert Values.js
@@ -5,7 +5,7 @@ invert([1,2,3,4,5]) == [-1,-2,-3,-4,-5]
 invert([1,-2,3,-4,5]) == [-1,2,-3,4,-5]
 invert([]) == []
 
-You can assume that all values are integers. Do not mutate the input array/list.
+You can assume that all values are integers. Do not mutate the input array/list
 */
 
 function invert(array) {
