6fe64f389043169925ccc0bc9d79c59d83b38cfb
GregTorrillo <99836116+GregTorrillo@users.noreply.github.com>
2022-12-09 20:53:20 -0500

    Update A wolf in sheep's clothing.js

--- 8kyu/A wolf in sheep's clothing.js
+++ 8kyu/A wolf in sheep's clothing.js
@@ -7,7 +7,7 @@ Warn the sheep in front of the wolf that it is about to be eaten. Remember that 
    7      6      5      4      3            2      1
 If the wolf is the closest animal to you, return "Pls go away and stop eating my sheep". Otherwise, return "Oi! Sheep number N! You are about to be eaten by a wolf!" where N is the sheep's position in the queue.
 
-Note: there will always be exactly one wolf in the array.
+Note: there will always be exactly one wolf in the array
 
 Example:
 Input: ["sheep", "sheep", "sheep", "wolf", "sheep"]
