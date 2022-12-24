7d0ba59e44892291ba10bcaf9db5e6370b3234c0
GregTorrillo <99836116+GregTorrillo@users.noreply.github.com>
2022-12-23 21:17:59 -0500

    Update Dollars and Cents.js

--- 8kyu/Dollars and Cents.js
+++ 8kyu/Dollars and Cents.js
@@ -5,7 +5,7 @@ The company you work for has just been awarded a contract to build a payment gat
 
 The rest of your team will make sure that the argument is sanitized before being passed to your function although you will need to account for adding trailing zeros if they are missing (though you won't have to worry about a dangling period).
 
-Ex:
+Ex
 
 3 needs to become $3.00
 
