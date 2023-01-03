d6ce17dccbaf50ff4042eb78eb7a89e3e795a052
GregTorrillo <99836116+GregTorrillo@users.noreply.github.com>
2023-01-03 18:02:48 -0500

    Update Target Date.js

--- 7kyu/Target Date.js
+++ 7kyu/Target Date.js
@@ -9,7 +9,7 @@ date_nb_days(100, 101, 0.98) --> "2017-01-01" (366 days)
 
 date_nb_days(100, 150, 2.00) --> "2035-12-26" (7299 days)
 
-Notes:
+Notes
 The return format of the date is "YYYY-MM-DD"
 The deposit is always on the "2016-01-01"
 Don't forget to take the rate for a day to be p divided by 36000 since banks consider that there are 360 days in a year.
