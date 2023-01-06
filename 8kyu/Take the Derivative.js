569ec56f1f97df4404a8e0c8eee0fbf60b5e3c26
GregTorrillo <99836116+GregTorrillo@users.noreply.github.com>
2023-01-05 19:11:20 -0500

    Update Take the Derivative.js

--- 8kyu/Take the Derivative.js
+++ 8kyu/Take the Derivative.js
@@ -3,7 +3,7 @@ This function takes two numbers as parameters, the first number being the coeffi
 
 Your function should multiply the two numbers, and then subtract 1 from the exponent. Then, it has to print out an expression (like 28x^7). "^1" should not be truncated when exponent = 2.
 
-For example:
+For example
 
 derive(7, 8)
 In this case, the function should multiply 7 and 8, and then subtract 1 from 8. It should output "56x^7", the first number 56 being the product of the two numbers, and the second number being the exponent minus 1.
