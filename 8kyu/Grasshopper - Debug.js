716a67edea1d6aeb6ab88793e16025520abb3e4d
GregTorrillo <99836116+GregTorrillo@users.noreply.github.com>
2023-01-12 18:02:35 -0500

    Update Grasshopper - Debug.js

--- 8kyu/Grasshopper - Debug.js
+++ 8kyu/Grasshopper - Debug.js
@@ -3,7 +3,7 @@ Your friend is traveling abroad to the United States so he wrote a program to co
 
 Find the errors in the code to get the celsius converter working properly.
 
-To convert fahrenheit to celsius:
+To convert fahrenheit to celsius
 
 celsius = (fahrenheit - 32) * (5/9)
 Remember that typically temperatures in the current weather conditions are given in whole numbers. It is possible for temperature sensors to report temperatures with a higher accuracy such as to the nearest tenth. Instrument error though makes this sort of accuracy unreliable for many types of temperature measuring sensors.
