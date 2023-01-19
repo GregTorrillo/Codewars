5416b50773104d57e2c6747359f4c0b4c5008ed8
GregTorrillo <99836116+GregTorrillo@users.noreply.github.com>
2023-01-19 15:50:50 -0500

    Update How good are you really?.js

--- 8kyu/How good are you really?.js
+++ 8kyu/How good are you really?.js
@@ -6,7 +6,7 @@ You receive an array with your peers' test scores. Now calculate the average and
 
 Return True if you're better, else False!
 
-Note:
+Note
 Your points are not included in the array of your class's points. For calculating the average point you may add your point to the given array!
 */
 
