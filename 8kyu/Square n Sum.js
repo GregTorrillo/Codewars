6cb00679c54db504b6e3fda7e58790c6a9ea6eb9
GregTorrillo <99836116+GregTorrillo@users.noreply.github.com>
2023-01-20 13:54:51 -0500

    Update Square n Sum.js

--- 8kyu/Square n Sum.js
+++ 8kyu/Square n Sum.js
@@ -1,4 +1,4 @@
-//Complete the square sum function so that it squares each number passed into it and then sums the results together.
+//Complete the square sum function so that it squares each number passed into it and then sums the results together
 
 function squareSum(numbers){
   let sum = 0
