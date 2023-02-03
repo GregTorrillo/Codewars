91ed98b6c31bff7c0eaf6f34a1fd3490f436de4a
GregTorrillo <99836116+GregTorrillo@users.noreply.github.com>
2023-02-03 15:52:59 -0500

    Update Polish alphabet.js

--- 8kyu/Polish alphabet.js
+++ 8kyu/Polish alphabet.js
@@ -1,7 +1,7 @@
 /*
 There are 32 letters in the Polish alphabet: 9 vowels and 23 consonants.
 
-Your task is to change the letters with diacritics:
+Your task is to change the letters with diacritics
 
 ą -> a,
 ć -> c,
